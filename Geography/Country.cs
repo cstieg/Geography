331526ef3e2534ac9026bcbbb4b7040ddc88af48
cstieg <cstieg4899@yahoo.com>
2018-01-18 11:42:44 -0500

    Add: Country

---
+++ Geography/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cstieg.Geography
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string IsoCode2 { get; set; }
+    }
+}
